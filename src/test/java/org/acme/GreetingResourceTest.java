7f88389e81a21b643fee68222f0a6f615c62eb97
Alejandro Ayuso <alejandroayuso@gmail.com>
2021-11-22 12:38:08 +0100

    Added more tests

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -12,6 +12,10 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class GreetingResourceTest {
 
+    /*
+    * Tests with no body
+    */
+
     @Test
     public void testBug_POST() {
         given()
@@ -36,6 +40,118 @@ public class GreetingResourceTest {
     public void testBug_PATCH() {
         given()
           .contentType(MediaType.APPLICATION_JSON_PATCH_JSON)
+          .when()
+          .patch("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    /*
+    * Tests with valid body
+    */
+    
+    @Test
+    public void testBug_POST_withValidBody() {
+        given()
+          .contentType(ContentType.JSON)
+          .body("{\"name\":\"foo\"}")
+          .when()
+          .post("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    @Test
+    public void testBug_PUT_withValidBody() {
+        given()
+          .contentType(ContentType.JSON)
+          .body("{\"name\":\"foo\"}")
+          .when()
+          .put("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    @Test
+    public void testBug_PATCH_withValidBody() {
+        given()
+          .contentType(MediaType.APPLICATION_JSON_PATCH_JSON)
+          .body("{\"op\":\"replace\", \"path\":\"/name\", \"value\":\"foo\"}")
+          .when()
+          .patch("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    /*
+    * Tests with invalid body
+    */
+    
+    @Test
+    public void testBug_POST_withInvalidBody() {
+        given()
+          .contentType(ContentType.JSON)
+          .body("{\"invalid\":\"foo\"}")
+          .when()
+          .post("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    @Test
+    public void testBug_PUT_withInvalidBody() {
+        given()
+          .contentType(ContentType.JSON)
+          .body("{\"invalid\":\"foo\"}")
+          .when()
+          .put("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    @Test
+    public void testBug_PATCH_withInvalidBody() {
+        given()
+          .contentType(MediaType.APPLICATION_JSON_PATCH_JSON)
+          .body("{\"op\":\"invalid\", \"path\":\"/name\", \"value\":\"foo\"}")
+          .when()
+          .patch("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    /*
+    * Tests with invalid content type.
+    * Not sure if the JWT should be first validated before the content type validation 
+    */
+    
+    @Test
+    public void testBug_POST_withInvalidContent() {
+        given()
+          .contentType(ContentType.TEXT)
+          .body("{\"invalid\":\"foo\"}")
+          .when()
+          .post("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    @Test
+    public void testBug_PUT_withInvalidContent() {
+        given()
+          .contentType(ContentType.TEXT)
+          .body("{\"invalid\":\"foo\"}")
+          .when()
+          .put("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    @Test
+    public void testBug_PATCH_withInvalidContent() {
+        given()
+          .contentType(ContentType.JSON)
+          .body("{\"op\":\"invalid\", \"path\":\"/name\", \"value\":\"foo\"}")
           .when()
           .patch("/hello/secured/bugged")
           .then()
