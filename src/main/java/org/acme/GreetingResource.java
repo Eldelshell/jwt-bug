bc645caca5270d4dc7e402feec6e0910da155f55
Alejandro Ayuso <alejandroayuso@gmail.com>
2021-11-22 10:47:59 +0100

    Add PUT/PATCH endpoints

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -10,16 +10,17 @@ import javax.ws.rs.Consumes;
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import javax.enterprise.context.ApplicationScoped;
-
+import javax.ws.rs.PATCH;
+import javax.ws.rs.PUT;
 
 @Path("/hello")
 @ApplicationScoped
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class GreetingResource {
 
     @POST
     @Path("/insecure")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public Response helloInsecure() {
         return Response.ok().build();
     }
@@ -27,6 +28,8 @@ public class GreetingResource {
     @POST
     @Path("/secured/works")
     @RolesAllowed({"User"})
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public Response helloSecured() {
         return Response.ok().build();
     }
@@ -34,7 +37,42 @@ public class GreetingResource {
     @POST
     @Path("/secured/bugged")
     @RolesAllowed({"User"})
-    public Response helloSecured(@Valid MyRequest req) {
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response helloSecuredPost(@Valid MyRequest req) {
+        System.out.println("POST You shouldn't be here");
+        System.out.println(req);
+        return Response.ok("{\"message\":\"you shouldn't be here\"}").build();
+    }
+    
+    @PATCH
+    @Path("/secured/bugged")
+    @RolesAllowed({"User"})
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
+    public Response helloSecuredPatch(@Valid MyRequest req) {
+        System.out.println("PATCH You shouldn't be here");
+        System.out.println(req);
+        return Response.ok("{\"message\":\"you shouldn't be here\"}").build();
+    }
+    
+    @PUT
+    @Path("/secured/bugged")
+    @RolesAllowed({"User"})
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response helloSecuredPut(@Valid MyRequest req) {
+        System.out.println("PUT You shouldn't be here");
+        System.out.println(req);
+        return Response.ok("{\"message\":\"you shouldn't be here\"}").build();
+    }
+
+    @POST
+    @Path("/secured/bugged-text")
+    @RolesAllowed({"User"})
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response helloSecured(String req) {
         System.out.println("You shouldn't be here");
         System.out.println(req);
         return Response.ok("{\"message\":\"you shouldn't be here\"}").build();
