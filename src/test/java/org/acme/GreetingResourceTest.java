b7ab9607b5705374ce652715ec82aaea8e8f87c4
Alejandro Ayuso <alejandroayuso@gmail.com>
2021-11-22 10:50:50 +0100

    Test bug

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -3,6 +3,8 @@ package org.acme;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
+import io.restassured.http.ContentType;
+import javax.ws.rs.core.MediaType;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -10,14 +12,34 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Disabled
     @Test
-    public void testHelloEndpoint() {
+    public void testBug_POST() {
         given()
-          .when().get("/hello")
+          .contentType(ContentType.JSON)
+          .when()
+          .post("/hello/secured/bugged")
           .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+          .statusCode(401);
+    }
+    
+    @Test
+    public void testBug_PUT() {
+        given()
+          .contentType(ContentType.JSON)
+          .when()
+          .put("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
+    }
+    
+    @Test
+    public void testBug_PATCH() {
+        given()
+          .contentType(MediaType.APPLICATION_JSON_PATCH_JSON)
+          .when()
+          .patch("/hello/secured/bugged")
+          .then()
+          .statusCode(401);
     }
 
 }
